35002a78954447d73bb4f7c5dbe2398ccff3c9ad
mr-teslaa <mr.teslaa@protonmail.com>
2022-01-26 18:19:52 +0600

    finished lesson

--- index.php
+++ index.php
@@ -1,17 +1,37 @@
+<!-- either you can echo any string in a php file, or you can echo that into a html tag like h1 or so -->
+<?php
+    define('NAME', 'JHONE DOE');
+    $name = "MR-TESLA";
+    echo 'Name form echo is: '.$name;
+?>
+
 <!DOCTYPE html>
+
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>First php file</title>
+    <style>
+        body {
+            font-family: sans-serif;
+        }
+    </style>
 </head>
-<body>
-    <h1>
-        <?php echo "allah is almighty"; ?>
-    </h1>
 
-    <!-- we can also do this -->
-    <?php echo "another echo"; ?>
+<body>
+
+    <h3>
+        Name from general variable is "<?php echo $name; ?>"
+    </h3>
+
+    <h3>
+        Name form constant variable is "<?php echo NAME; ?>"
+    </h3>
+
 </body>
+
+
 </html>
--- readme.md
+++ readme.md
@@ -1,3 +1,13 @@
-# Lesson 0: First php file
+# Lesson 1: Variables and Constants
+
+-   `Variables`: There are 2 types of variables in php.
+
+    1. General - we can change it later on if we want to.
+
+    -   Syntax is `$variable_name = value` without number & special characters
+
+    2. Constant - we can't change this variable value.
+
+    -   Syntax is `$variable_name = value` without number & special characters
 
 _I created separate brach of each lesson_
